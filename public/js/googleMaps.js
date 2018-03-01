d521d7e5a9076645613aabf3fb8832d3bec4f1d9
Neda Khanverdi <nkhanver@ucsd.edu>
2018-03-01 01:32:47 -0800

    new

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,6 +1,6 @@
 function initMap() {
 	// add your code here
-	var ucsd_ltlng = {lat:32.880, lng:-117.236};
+	
 	var ssc = {lat:32.879012, lng:-117.235844};
 
 	var map = new google.maps.Map(document.getElementById('map'), {
